886e962e115f9d8980d050df16ee4702302126a3
EmirSiner <emirhansiner@gmail.com>
2022-08-10 20:41:30 +0300

    Fix retrofit provide method

--- app/src/main/java/com/example/di/NetworkModule.kt
+++ app/src/main/java/com/example/di/NetworkModule.kt
@@ -5,13 +5,15 @@ import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class NetworkModule {
 
     @Provides
-    fun networkModule(){
-    Retrofit.Builder()
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+    return Retrofit.Builder()
     .baseUrl("http://hp-api.herokuapp.com/api/")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
