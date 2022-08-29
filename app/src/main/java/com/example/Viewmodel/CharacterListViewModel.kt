32f7e7c4d65c568925cfba486b274f31494fa917
EmirSiner <emirhansiner@gmail.com>
2022-08-29 10:51:32 +0300

    inject Viewmodel

--- app/src/main/java/com/example/Viewmodel/CharacterListViewModel.kt
+++ app/src/main/java/com/example/Viewmodel/CharacterListViewModel.kt
@@ -1,12 +1,17 @@
-package com.example.harrypotterapp.character
+package com.example.Viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.di.CharacterService
+import com.example.retrofit.HpCharacterResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import retrofit2.Retrofit
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterListViewModel @Inject constructor(private val characterService: CharacterService): ViewModel() {
+    private val hpLiveData= MutableLiveData<HpCharacterResponse>()
+    val hpCharacterLiveData:LiveData<HpCharacterResponse> = hpLiveData
 
 }
